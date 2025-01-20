0da3697b4226ebf43480fdb1c6a50dee5eb58e34
Harrison-Holt <hholt2901@gmail.com>
2025-01-20 10:02:14 -0500

    Working on Homepage

--- src/UI/Home.jsx
+++ src/UI/Home.jsx
@@ -14,11 +14,16 @@ const Home = () => {
             </AppBar>
 
             { /* Main Content */ }
-            <Box sx={{ minHeight: '100vh', backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+            <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center', marginTop: '64px'}}>
                 <Typography variant='h2' sx={{ color: '#000'}}>Home</Typography>
-                <Paper>
+            <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}> 
+                <Paper sx={{ width: '500px', height: '500px'}}>
                     <Typography variant='body1'>Card #1</Typography>
                 </Paper>
+                <Paper sx={{ width: '500px', height: '500px'}}>
+                    <Typography variant='body1'>Card #1</Typography>
+                </Paper>
+            </Box>
             </Box>
 
             <Footer />

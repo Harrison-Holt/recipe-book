e8562763b2b9f4aa8bd8f7c32fdae41a00a67e9d
Harrison-Holt <hholt2901@gmail.com>
2025-01-20 10:06:42 -0500

    Working on Homepage

--- src/UI/Home.jsx
+++ src/UI/Home.jsx
@@ -1,6 +1,9 @@
 import Footer from './Footer.jsx'
 import Background from '../../public/assets/kitchen_background.jpg'
+import current_recipes from '../../public/assets/current_reciepes_background.jpg'
+import my_recipes from '../../public/assets/my_recipes_background.jpg'
 import { AppBar, Toolbar, Typography, Box, Paper } from '@mui/material'
+
 const Home = () => {
 
     return (
@@ -15,12 +18,11 @@ const Home = () => {
 
             { /* Main Content */ }
             <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center', marginTop: '64px'}}>
-                <Typography variant='h2' sx={{ color: '#000'}}>Home</Typography>
             <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}> 
-                <Paper sx={{ width: '500px', height: '500px'}}>
+                <Paper sx={{ width: '500px', height: '500px', backgroundImage: `url(${current_recipes})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
                     <Typography variant='body1'>Card #1</Typography>
                 </Paper>
-                <Paper sx={{ width: '500px', height: '500px'}}>
+                <Paper sx={{ width: '500px', height: '500px', backgroundImage: `url(${my_recipes})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
                     <Typography variant='body1'>Card #1</Typography>
                 </Paper>
             </Box>

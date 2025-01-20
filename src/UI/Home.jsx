5e98ea344d2037a8e047d15a6198b18271faa145
Harrison-Holt <hholt2901@gmail.com>
2025-01-20 18:38:53 -0500

    working Homepage

--- src/UI/Home.jsx
+++ src/UI/Home.jsx
@@ -20,12 +20,12 @@ const Home = () => {
             <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center', marginTop: '64px'}}>
             <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}> 
                 <Paper sx={{ width: '350px', height: '400px', backgroundImage: `url(${current_recipes})`, backgroundSize: 'cover', backgroundPosition: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                    <Typography variant='h4' sx={{ marginBottom: '20px' }}>Card #1</Typography>
-                    <Button variant='contained'>View Current Recipes</Button>
+                    <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
+                    <Button variant='contained' sx={{ backgroundColor: '#000'}}>View Current Recipes</Button>
                 </Paper>
                 <Paper sx={{ width: '350px', height: '400px', backgroundImage: `url(${my_recipes})`, backgroundSize: 'cover', backgroundPosition: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                    <Typography variant='h4' sx={{ marginBottom: '20px', color: 'red'}}>Card #2</Typography>
-                    <Button variant='contained'>View My Recipes</Button>
+                    <Typography variant='h4' sx={{ marginBottom: '20px', color: '#000', backgroundColor: '#fff'}}>Card #2</Typography>
+                    <Button variant='contained'  sx={{ backgroundColor: '#000'}}>View My Recipes</Button>
                 </Paper>
             </Box>
             </Box>

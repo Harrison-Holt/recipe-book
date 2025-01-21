2443add651b53ce00a36e471f079ae7e9aa9e97c
Harrison-Holt <hholt2901@gmail.com>
2025-01-20 19:23:50 -0500

    working Homepage

--- src/UI/Featured_Recipes.jsx
+++ src/UI/Featured_Recipes.jsx
@@ -3,7 +3,7 @@ import {  Typography, Paper, Button, Grid } from '@mui/material'
 const FeaturedReacipes = () => {
 
     return (
-        <Grid container columns={2} spacing={2} sx={{ display: 'flex', alignItems: 'center'}}>
+        <Grid container columns={2} spacing={2}       sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
         <Grid xs={1} item>
         <Paper sx={{ width: '350px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
         <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>

aa719975f4cd2d2601e869e3b43f861cc7d0e595
Harrison-Holt <hholt2901@gmail.com>
2025-01-21 13:08:44 -0500

    working on Footer

--- src/UI/Footer.jsx
+++ src/UI/Footer.jsx
@@ -5,9 +5,13 @@ const Footer = () => {
     return (
     <>
     <Box sx={{ backgroundColor: '#ECEDEF'}}>
-        <Toolbar sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center'}}>
-            <Button variant='contained' sx={{ backgroundColor: '#000', color: '#fff' }}>Settings</Button>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', width: '50%'}}>
+            <Box>
             <Typography variant='body1' sx={{ color: '#000', fontFamily: 'merriweather'}}>&copy;2025 FlavorCraft</Typography>
+            </Box>
+            <Box>
+            <Button variant='contained' sx={{ backgroundColor: '#000', color: '#fff' }}>Settings</Button>
+            </Box>
         </Toolbar>
     </Box>
     </>

c9ba85eb65e2bd5999e67d001efaa9dad98c8807
Harrison-Holt <hholt2901@gmail.com>
2025-01-20 22:12:09 -0500

    working Homepage

--- src/UI/Featured_Recipes.jsx
+++ src/UI/Featured_Recipes.jsx
@@ -4,26 +4,26 @@ const FeaturedReacipes = () => {
 
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-        <Grid container columns={2} spacing={1}       sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-        <Grid xs={12} sm={6} md={6}>
+        <Grid container columns={2} spacing={1} sx={{ minHeight: '100vh' }}>
+        <Grid xs={12} sm={6} md={6} item>
         <Paper sx={{ width: '350px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
         <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
         <Button variant='contained' sx={{ backgroundColor: '#2EC698'}}>View Current Recipes</Button>
         </Paper>
         </Grid>
-        <Grid xs={12} sm={6} md={6}>
+        <Grid xs={12} sm={6} md={6} item>
         <Paper sx={{ width: '350px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
         <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
         <Button variant='contained' sx={{ backgroundColor: '#2EC698'}}>View Current Recipes</Button>
         </Paper>
         </Grid>
-        <Grid xs={12} sm={6} md={6}>
+        <Grid xs={12} sm={6} md={6} item>
         <Paper sx={{ width: '350px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
         <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
         <Button variant='contained' sx={{ backgroundColor: '#2EC698'}}>View Current Recipes</Button>
         </Paper>
         </Grid>
-        <Grid xs={12} sm={6} md={6}>
+        <Grid xs={12} sm={6} md={6} item>
         <Paper sx={{ width: '350px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
         <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
         <Button variant='contained' sx={{ backgroundColor: '#2EC698'}}>View Current Recipes</Button>

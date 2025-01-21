3fbbbdf385bdb108615ad915c7388ce53248b450
Harrison-Holt <hholt2901@gmail.com>
2025-01-20 19:03:31 -0500

    working Homepage

---
+++ src/UI/Featured_Recipes.jsx
@@ -0,0 +1,35 @@
+import {  Typography, Paper, Button, Grid } from '@mui/material'
+
+const FeaturedReacipes = () => {
+
+    return (
+        <Grid container columns={2} spacing={2}>
+        <Grid xs={1} item>
+        <Paper sx={{ width: '350px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
+        <Button variant='contained' sx={{ backgroundColor: '#2EC698'}}>View Current Recipes</Button>
+        </Paper>
+        </Grid>
+        <Grid xs={1} item>
+        <Paper sx={{ width: '350px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
+        <Button variant='contained' sx={{ backgroundColor: '#2EC698'}}>View Current Recipes</Button>
+        </Paper>
+        </Grid>
+        <Grid xs={1} item>
+        <Paper sx={{ width: '350px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
+        <Button variant='contained' sx={{ backgroundColor: '#2EC698'}}>View Current Recipes</Button>
+        </Paper>
+        </Grid>
+        <Grid xs={1} item>
+        <Paper sx={{ width: '350px', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
+        <Button variant='contained' sx={{ backgroundColor: '#2EC698'}}>View Current Recipes</Button>
+        </Paper>
+        </Grid>
+    </Grid>
+    )
+}
+
+export default FeaturedReacipes
--- src/UI/Home.jsx
+++ src/UI/Home.jsx
@@ -3,6 +3,7 @@ import Background from '../../public/assets/kitchen_background.jpg'
 import current_recipes from '../../public/assets/current_reciepes_background.jpg'
 import my_recipes from '../../public/assets/my_recipes_background.jpg'
 import { AppBar, Toolbar, Typography, Box, Paper, Button } from '@mui/material'
+import FeaturedReacipes from './Featured_Recipes.jsx'
 
 const Home = () => {
 
@@ -17,19 +18,23 @@ const Home = () => {
             </AppBar>
 
             { /* Main Content */ }
-            <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center', marginTop: '64px'}}>
+            <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center', marginTop: '64px'}}>
             <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}> 
                 <Paper sx={{ width: '350px', height: '400px', backgroundImage: `url(${current_recipes})`, backgroundSize: 'cover', backgroundPosition: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
                     <Typography variant='h4' sx={{ marginBottom: '20px', backgroundColor: '#fff'}}>Card #1</Typography>
-                    <Button variant='contained' sx={{ backgroundColor: '#000'}}>View Current Recipes</Button>
+                    <Button variant='contained' sx={{ backgroundColor: '#2EC698'}}>View Current Recipes</Button>
                 </Paper>
                 <Paper sx={{ width: '350px', height: '400px', backgroundImage: `url(${my_recipes})`, backgroundSize: 'cover', backgroundPosition: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
                     <Typography variant='h4' sx={{ marginBottom: '20px', color: '#000', backgroundColor: '#fff'}}>Card #2</Typography>
-                    <Button variant='contained'  sx={{ backgroundColor: '#000'}}>View My Recipes</Button>
+                    <Button variant='contained'  sx={{ backgroundColor: '#2EC698'}}>View My Recipes</Button>
                 </Paper>
             </Box>
-            </Box>
 
+            <Typography variant='h3' sx={{ fontSize: '32px', color: '#000'}}>Featured Recipes</Typography>
+            
+                <FeaturedReacipes />
+            </Box>
+    
             <Footer />
         </Box>
     )

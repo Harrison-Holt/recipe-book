2a7c623dfe34a9a9b34f2c46f8d1227aa5a2ca3a
Harrison-Holt <hholt2901@gmail.com>
2025-01-31 11:14:52 -0500

    updated aws cognito file

--- src/aws_config/Cognito.js
+++ src/aws_config/Cognito.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: 'us-east-1_aLN62XR1m',
-    ClientId: '62thbei4nhiivplj5vdiafn26i'
+    UserPoolId: 'us-east-1_AkRD8bJUN',
+    ClientId: '42qk69ucev7p5j7jt7dpjtlisa'
 }
 
 const  userPool = new CognitoUserPool(poolData); 
